e9419e844f6edc0261e90a6227ac0a861c6f7f0c
Joshua Emmons <joshua@reverb.com>
2015-03-07 13:24:50 -0600

    Checkin project.

---
+++ Result/Result.h
@@ -0,0 +1,19 @@
+//
+//  Result.h
+//  Result
+//
+//  Created by Joshua Emmons on 3/7/15.
+//  Copyright (c) 2015 MCMXCIX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Result.
+FOUNDATION_EXPORT double ResultVersionNumber;
+
+//! Project version string for Result.
+FOUNDATION_EXPORT const unsigned char ResultVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Result/PublicHeader.h>
+
+
---
+++ Result/Result.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+public class Result{
+  public var foo:String{
+    return "Foo"
+  }
+  
+  public init(){
+    
+  }
+}
